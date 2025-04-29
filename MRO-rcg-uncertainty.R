19b734c53393e9e44eb6a87a4993ac06f8f54f88
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2025-04-29 11:43:03 +0200

    Add HyperTraPS clone; just 1 seed for uncertainty analysis

--- MRO-rcg-uncertainty.R
+++ MRO-rcg-uncertainty.R
@@ -9,7 +9,6 @@ if (length(args)!=4) {
 
 library(phytools)
 library(parallel)
-library(sybilSBML)
 library(ggplot2)
 
 treefile <- as.character(args[1])
@@ -46,7 +45,7 @@ set.seed(1234)
 data.ncbi = curate.tree(tree, df, losses=TRUE)
 
 # run these experiments in parallel. should take a few core minutes each
-n.seed = 5
+n.seed = 1
 featurenames = colnames(df)[2:length(colnames(df))]
 parallelised.runs <- mcmapply(parallel.fn, seed=1:n.seed,
                               MoreArgs = list(src.data=data.ncbi,
@@ -128,137 +127,3 @@ png(paste(outfolder, "mro-pathways-", tag, ".png", sep = ""), width=600*sf, heig
 print(g.pathways)
 dev.off()
 
-fba.mod = readSBMLmod("FBA/MitoMammal/MitoMammal.xml")
-etc.inds <- c(98,108:112)
-etc.rs = c("PDH","CI","CII","CIII","CIV","CV")#fba.mod@react_name[etc.inds]
-EX_o2 <- 50
- 
-
-atp.obj = rep(0, length(fba.mod@react_name))
-atp.obj[71] = 1
-res.df = data.frame(name = NULL, ko.val = NULL, ko.atp.val = NULL, ko.val.hypoxia = NULL, ko.atp.val.hypoxia = NULL)
-for(ko in 1:length(etc.rs)) {
-  tmp.mod = fba.mod
-  tmp.mod@lowbnd[etc.rs[ko]] = 0
-  tmp.mod@uppbnd[etc.rs[ko]] = 0
-  soln = optimizeProb(tmp.mod)
-  soln.atp = optimizeProb(tmp.mod, obj_coef = atp.obj)
-  tmp.mod@lowbnd[EX_o2] = 0
-  tmp.mod@uppbnd[EX_o2] = 1
-  soln.hypoxia = optimizeProb(tmp.mod)
-  soln.atp.hypoxia = optimizeProb(tmp.mod, obj_coef = atp.obj)
-  res.df = rbind(res.df, data.frame(name=etc.rs[ko], 
-                                    ko.val=soln@lp_obj, ko.atp.val=soln.atp@lp_obj,
-                                    ko.val.hypoxia=soln.hypoxia@lp_obj,
-                                    ko.atp.val.hypoxia=soln.atp.hypoxia@lp_obj))
-}
-
-tmp.mod = fba.mod
-tmp.mod@lowbnd[98:107] = 0
-tmp.mod@uppbnd[98:107] = 0
-soln = optimizeProb(tmp.mod)
-soln.atp = optimizeProb(tmp.mod, obj_coef = atp.obj)
-tmp.mod@lowbnd[EX_o2] = 0
-tmp.mod@uppbnd[EX_o2] = 1
-soln.hypoxia = optimizeProb(tmp.mod)
-soln.atp.hypoxia = optimizeProb(tmp.mod, obj_coef = atp.obj)
-res.df = rbind(res.df, data.frame(name="TCA", 
-                                  ko.val=soln@lp_obj, ko.atp.val=soln.atp@lp_obj,
-                                  ko.val.hypoxia=soln.hypoxia@lp_obj,
-                                  ko.atp.val.hypoxia=soln.atp.hypoxia@lp_obj))
-
-tmp.mod = fba.mod
-soln = optimizeProb(tmp.mod)
-soln.atp = optimizeProb(tmp.mod, obj_coef = atp.obj)
-tmp.mod@lowbnd[EX_o2] = 0
-tmp.mod@uppbnd[EX_o2] = 1
-soln.hypoxia = optimizeProb(tmp.mod)
-soln.atp.hypoxia = optimizeProb(tmp.mod, obj_coef = atp.obj)
-res.df = rbind(res.df, data.frame(name="full model", 
-                                  ko.val=soln@lp_obj, ko.atp.val=soln.atp@lp_obj,
-                                  ko.val.hypoxia=soln.hypoxia@lp_obj,
-                                  ko.atp.val.hypoxia=soln.atp.hypoxia@lp_obj))
-
-write.csv(res.df, "fba-res.csv")
-res.df = read.csv("fba-res.csv")
-
-#print(res.df)
-
-#ggplot(res.df, aes(x=ko.atp.val, y=ko.atp.val.hypoxia, label=label)) + geom_point() + ggrepel::geom_text_repel()
-
-#ggplot(res.df, aes(x=ko.val, y=ko.val.hypoxia, label=label)) + 
-#  geom_point() + 
-#  ggrepel::geom_text_repel()
-# First-loss probability vs metabolic impact from FBA in the normoxic case
-
-# Labelling for easy access to elements
-res.df$label = ""
-res.df$label[1:7]  = c("PDH","CI","CII","CIII","CIV","CV","TCA")
-#res.df$label[2] = "CI"
-#res.df$label[3] = "CII"
-#res.df$label[4] = "CIII"
-#res.df$label[5] = "CIV"
-#res.df$label[6] = "CV"
-#res.df$label[7] = "TCA"
-
-first.steps = parallelised.runs[[1]]$routes[,1]
-first.df = res.df[res.df$label!="",]
-#print(first.df)
-first.df$propn = 0
-for(i in 1:nrow(first.df)) {
-  ref = which(featurenames == first.df$label[i])-1
-  propn = sum(first.steps == ref) / length(first.steps)
-  first.df$propn[i] = propn
-}
-g.normoxic = ggplot(first.df, aes(x=ko.atp.val, y=propn, label=label)) + 
-  geom_point() + geom_smooth(method="lm", color="#AAAAFF", fill="#AAAAFF") + 
-  ggrepel::geom_text_repel() + theme_light() +
-  labs(x = "ATP objective on KO", y = "First loss probability")
-
-#g.normoxic
-fit.lm = summary(lm(propn~ko.atp.val, data=first.df))
-
-cs = fit.lm$coefficients
-tstr = sprintf("Slope %.1e +- %.1e, p=%.2e", cs[2,1], cs[2,2], cs[2,4])
-
-png(paste(outfolder, "mro-first-fba-normoxic-", tag, ".png", sep = ""), width=300*sf, height=300*sf, res=72*sf)
-print(g.normoxic + ggtitle(tstr))
-dev.off()
-
-# First-loss probability vs metabolic impact from FBA in the anoxic case
-
-# Re-labelling for easy access
-res.df$label = ""
-res.df$label[1:7]  = c("PDH","CI","CII","CIII","CIV","CV","TCA")
-#res.df$label[2] = "CI"
-#res.df$label[3] = "CII"
-#res.df$label[4] = "CIII"
-#res.df$label[5] = "CIV"
-#res.df$label[6] = "CV"
-#res.df$label[7] = "TCA"
-
-first.steps = parallelised.runs[[1]]$routes[,1]
-first.df = res.df[res.df$label!="",]
-#print(first.df)
-first.df$propn = 0
-for(i in 1:nrow(first.df)) {
-  ref = which(featurenames == first.df$label[i])-1
-  propn = sum(first.steps == ref) / length(first.steps)
-  first.df$propn[i] = propn
-}
-g.anoxic = ggplot(first.df, aes(x=ko.atp.val.hypoxia, y=propn, label=label)) + 
-  geom_point() + geom_smooth(method="lm", color="#AAAAFF", fill="#AAAAFF") + 
-  ggrepel::geom_text_repel() + theme_light() +
-  labs(x = "ATP objective on KO", y = "First loss probability")
-
-#g.anoxic
-fit.lm = summary(lm(propn~ko.atp.val.hypoxia, data=first.df))
-
-cs = fit.lm$coefficients
-tstr = sprintf("Slope %.1e +- %.1e, p=%.2e", cs[2,1], cs[2,2], cs[2,4])
-
-png(paste(outfolder, "mro-first-fba-anoxic-", tag, ".png",sep = ""), width=300*sf, height=300*sf, res=72*sf)
-print(g.anoxic + ggtitle(tstr))
-dev.off()
-
-save.image(paste(outfolder, "mro-save-", tag,".RData",sep = ""))
